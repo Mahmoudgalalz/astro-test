36cdabef3cfb0603a4bb9f2d7ae39103ddcf514b
Mahmoud Galal <42272376+Mahmoudgalalz@users.noreply.github.com>
2023-01-06 03:02:19 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare({mode: "directory"})
